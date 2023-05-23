a318aa6edbcc131f6909d5fe225b6882aa534c1d
Adam <adamgjohnson92@gmail.com>
2023-05-23 14:49:31 -0700

    Confirmed latitude and logitude data being received from geocode API request with console.log. Added setItem functionality.

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,24 @@
+var aside = document.querySelector("aside")
 var submitBtnEl = document.querySelector("#form")
 var userInputEl = document.querySelector("#userInput")
 var searchedCity = document.querySelector("#searchedCity")
-var preSearchedCities = []
+
 
 function getApiTest() {
-    var latLonCoordsURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=42c66a48a76a8c63ca42a8a780c249a4";
+    var latLonCoordsURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInputEl.value +"&limit=1&appid=42c66a48a76a8c63ca42a8a780c249a4";
 
     fetch(latLonCoordsURL)
     .then(function(response){
-        console.log(response)
         return response.json()
         
     }) .then(function(data){
         console.log(data)
+        for (var i = 0; i < data.length; i++) {
+            console.log(data[i].lat);
+            console.log(data[i].lon)
+          }
     })
 }
-
-getApiTest()
 
 // function getApi() {
 //     var latLonCoords = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityNameEl + "&limit=5&appid=42c66a48a76a8c63ca42a8a780c249a4";
@@ -24,12 +26,33 @@ getApiTest()
 //     var requestUrl = "api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&appid=42c66a48a76a8c63ca42a8a780c249a4"
 // }
 
+var preSearchedCities = JSON.parse(localStorage.getItem("cityName")) || []
 
 function submitHandler(event){
     event.preventDefault()
     searchedCity.textContent = userInputEl.value
     console.log(userInputEl.value)
-    console.log(10)
+    preSearchedCities.push(userInputEl.value)
+    saveSearchedCity()
+    getApiTest()
 }
+
+// function displayPreSearchedCities(){
+//     for (var i = 0; i < preSearchedCities.length; i++) {
+//         var cityButton = document.createElement("button")
+//         var cityButtonText = document.createTextNode(localStorage.getItem(preSearchedCities[i])) 
+//         cityButton = appendChild(cityButtonText)
+//         aside = appendChild(cityButton)
+//       } 
+// }
+
+//displayPreSearchedCities()
+
+function saveSearchedCity(){
+    
+    
+    localStorage.setItem("cityName", JSON.stringify(preSearchedCities))
+    console.log(preSearchedCities)
+    }
 
 submitBtnEl.addEventListener("submit", submitHandler)
