f792e376e5ea8689a0123441dc653f7867f05834
Adam <adamgjohnson92@gmail.com>
2023-05-24 17:45:01 -0700

    Fixed issue with dynamically created buttons of previously searched cities not displaying. City name still does not display in the button, but the buttons are created.

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ var geoLongitude = ""
 function getAPITest() {
     var latLonCoordsURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInputEl.value +"&limit=1&appid=42c66a48a76a8c63ca42a8a780c249a4";
 
-    var weatherUrl = "api.openweathermap.org/data/2.5/forecast?lat=" + geoLatitude + "&lon=" + geoLongitude + "&appid=42c66a48a76a8c63ca42a8a780c249a4"
+    
 
     fetch(latLonCoordsURL)
     .then(function(response){
@@ -30,6 +30,8 @@ function getAPITest() {
         // .then (function(data){
         //     console.log(data)
         // })
+
+var weatherUrl = "api.openweathermap.org/data/2.5/forecast/daily?lat=" + geoLatitude + "&lon=" + geoLongitude + "&appid=42c66a48a76a8c63ca42a8a780c249a4"
 
     fetch(weatherUrl)
     .then(function(response){
@@ -59,16 +61,16 @@ function submitHandler(event){
     getAPITest()
 }
 
-// function displayPreSearchedCities(){
-//     for (var i = 0; i < preSearchedCities.length; i++) {
-//         var cityButton = document.createElement("button")
-//         var cityButtonText = document.createTextNode(localStorage.getItem(preSearchedCities[i])) 
-//         cityButton = appendChild(cityButtonText)
-//         aside = appendChild(cityButton)
-//       } 
-// }
+function displayPreSearchedCities(){
+    for (var i = 0; i < preSearchedCities.length; i++) {
+        var cityButton = document.createElement("button")
+        var cityButtonText = document.createTextNode(localStorage.getItem(preSearchedCities[i])) 
+        cityButton.appendChild(cityButtonText)
+        aside.appendChild(cityButton)
+      } 
+}
 
-//displayPreSearchedCities()
+displayPreSearchedCities()
 
 function saveSearchedCity(){
     
